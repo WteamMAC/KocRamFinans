9499031a89d80ad06f057f7ad43b2bca7c7c059f
Murat <148049804+MuratEfeS@users.noreply.github.com>
2026-05-14 23:39:16 +0300

    Döviz hesaplayıcı eklendi ve hatalar giderildi

--- src/components/dashboard/compact-currency-calculator.tsx
+++ src/components/dashboard/compact-currency-calculator.tsx
@@ -104,7 +104,7 @@ export function CompactCurrencyCalculator() {
       </div>
       
       {/* From Currency Select */}
-      <Select value={fromCurrency} onValueChange={setFromCurrency}>
+      <Select value={fromCurrency} onValueChange={(v) => v && setFromCurrency(v)}>
         <SelectTrigger className="w-[50px] h-8 bg-transparent border-none focus:ring-0 text-[10px] font-black p-0 flex justify-center hover:text-primary transition-colors z-10">
           <SelectValue />
         </SelectTrigger>
@@ -129,7 +129,7 @@ export function CompactCurrencyCalculator() {
       </Button>
 
       {/* To Currency Select */}
-      <Select value={toCurrency} onValueChange={setToCurrency}>
+      <Select value={toCurrency} onValueChange={(v) => v && setToCurrency(v)}>
         <SelectTrigger className="w-[50px] h-8 bg-transparent border-none focus:ring-0 text-[10px] font-black p-0 flex justify-center hover:text-primary transition-colors z-10">
           <SelectValue />
         </SelectTrigger>
